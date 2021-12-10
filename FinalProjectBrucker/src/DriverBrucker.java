fee61d941d455d55a84e87fcf4c8160f07794072
cbrucker19 <89558460+cbrucker19@users.noreply.github.com>
2021-12-10 13:13:28 -0600

    Updated Driver with Academic Honestly Letter

--- FinalProjectBrucker/src/DriverBrucker.java
+++ FinalProjectBrucker/src/DriverBrucker.java
@@ -1,3 +1,19 @@
+/***************************************************************
+* Name : SkateShop Rewards Program
+* Author: Cyrus Brucker
+* Created : 12/10/2021
+* Course: CIS 152 - Data Structure
+* Version: 1.0
+* OS: MAC IOS
+* IDE: eclipse IDE,)
+* Copyright : This is my own original work 
+* based on specifications issued by our instructor
+* Description : Takes customer information and inputs it inside a rewards program to give certain customers deals with datastructes and sorting algorithms
+* Academic Honesty: I attest that this is my original work.
+* I have not used unauthorized source code, either modified or
+* unmodified. I have not given other fellow student(s) access
+* to my program.
+***************************************************************/
 import java.util.Scanner;
 public class DriverBrucker{
 	public static void main(String[] args) throws StackEmptyException, StackFullException {
